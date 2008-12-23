47d5a5031585fa78780d6778d36625fa32959a06
Brian Knox <gnutse@gmail.com>
2008-12-23 16:05:01 -0500

    reworking db config

--- main.rb
+++ main.rb
@@ -4,36 +4,39 @@ require 'dm-core'
 require 'dm-validations'
 require 'sass'
 require 'logger'
+require 'yaml'
 
 Dir.glob(File.join(File.dirname(__FILE__), 'models/*.rb')).each {|f| require f }
+DBCONFIG = YAML.load_file('database.yml') unless defined?(DBCONFIG)
+
+configure :test do
+  DataMapper.setup(:default, {
+    :host     => DBCONFIG['test']['host'], 
+    :adapter  => DBCONFIG['test']['adapter'],
+    :database => DBCONFIG['test']['database'],
+    :username => DBCONFIG['test']['username'],
+    :password => DBCONFIG['test']['password'] 
+    })
+end
 
 configure :development do
   DataMapper.setup(:default, {
-    :host     => 'localhost', 
-    :adapter  => 'mysql',
-    :database => 'wordnet30_development',
-    :username => 'rails',
-    :password => 'rails4242' })
-    
-  DataMapper::Logger.new(STDOUT, :debug)
+    :host     => DBCONFIG['development']['host'], 
+    :adapter  => DBCONFIG['development']['adapter'],
+    :database => DBCONFIG['development']['database'],
+    :username => DBCONFIG['development']['username'],
+    :password => DBCONFIG['development']['password'] 
+    })
 end
 
 configure :production do
   DataMapper.setup(:default, {
-    :host     => 'localhost', 
-    :adapter  => 'mysql',
-    :database => 'wordnet30',
-    :username => 'rails',
-    :password => 'rails4242' })
-end
-
-configure :test do
-  DataMapper.setup(:default, {
-    :host     => 'localhost', 
-    :adapter  => 'mysql',
-    :database => 'wordnet30_development',
-    :username => 'rails',
-    :password => 'rails4242' })
+    :host     => DBCONFIG['production']['host'], 
+    :adapter  => DBCONFIG['production']['adapter'],
+    :database => DBCONFIG['production']['database'],
+    :username => DBCONFIG['production']['username'],
+    :password => DBCONFIG['production']['password'] 
+    })
 end
 
 get '/' do
