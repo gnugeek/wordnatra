6a9e6aa49961ef1f445c948a14f2e29b6295cc9f
Brian Knox <gnutse@gmail.com>
2008-12-23 18:41:12 -0500

    fixing sample sql

--- docs/sample_queries.sql
+++ docs/sample_queries.sql
@@ -2,6 +2,9 @@ select
 sense.wordid,
 wordposition.positionid,
 word.lemma,
+synset.definition,
+sample.sample,
+categorydef.name,
 sentenceref.sentenceid,
 sentencedef.sentence,
 frameref.frameid,
@@ -12,8 +15,10 @@ casedword.lemma as casedword_lemma
 from
 sense
 inner join word on sense.wordid = word.wordid
-inner join wordposition on sense.wordid = wordposition.wordid and sense.synsetid = wordposition.synsetid
+left outer join  wordposition on sense.wordid = wordposition.wordid and sense.synsetid = wordposition.synsetid
 inner join synset on sense.synsetid = synset.synsetid
+left outer join sample on synset.synsetid = sample.synsetid
+left outer join categorydef on synset.categoryid = categorydef.categoryid
 left outer join casedword on sense.wordid = casedword.wordid
 left outer join sentenceref on sense.synsetid = sentenceref.synsetid and sense.wordid = sentenceref.wordid
 left outer join sentencedef on sentenceref.sentenceid = sentencedef.sentenceid
@@ -21,9 +26,9 @@ left outer join frameref on sense.synsetid = frameref.synsetid and sense.wordid 
 left outer join framedef on frameref.frameid = framedef.frame 
 left outer join morphref on word.wordid = morphref.wordid
 left outer join morphdef on morphref.morphid = morphdef.morphid
-left outer join synset on sense.synsetid = sy
 
-where morphdef.lemma is not null
+
+where word.lemma = 'love'
 
 
 ;
