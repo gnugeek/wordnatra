4f9a895331e262708203f797aed25545557836bc
Brian Knox <gnutse@gmail.com>
2008-12-26 14:41:48 -0500

    adding ordering to some assocatiations

--- models/word.rb
+++ models/word.rb
@@ -3,6 +3,6 @@ class Word
   storage_names[:default]='word'
   property :wordid, Integer, :key => true, :writer => :protected
   property :lemma,  String, :writer => :protected
-  has n, :senses,   :child_key => [:wordid]
-  has n, :synsets,  :through => :senses, :child_key => [:wordid]
+  has n, :senses,   :child_key => [:wordid], :order => [:rank.asc, :synsetid.asc]
+  has n, :synsets,  :through => :senses, :child_key => [:wordid], :order => [:synsetid.asc]
 end
