08274681370433a76c109abff7f610050e1e1494
Brian Knox <gnutse@gmail.com>
2008-12-26 14:32:17 -0500

    working on display of results

--- main.rb
+++ main.rb
@@ -13,6 +13,16 @@ get '/word/:lemma' do
   haml :word
 end
 
+get '/word/:lemma/to_json' do
+  @word = Word.first(:lemma => params[:lemma])
+  "#{@word.to_json}"
+end
+
+get '/word/:lemma/to_xml' do
+  @word = Word.first(:lemma => params[:lemma])
+  "#{@word.to_xml}"
+end
+
 get '/stylesheets/style.css' do
   header 'Content-Type' => 'text/css; charset=utf-8'
   sass :style
