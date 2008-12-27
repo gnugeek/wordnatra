f7273b05b0ac569acaaa9c1143c5c6689f1c92d5
Brian Knox <gnutse@gmail.com>
2008-12-27 14:37:26 -0500

    ui work

--- main.rb
+++ main.rb
@@ -13,16 +13,6 @@ get '/word/:lemma' do
   erb :word
 end
 
-get '/word/:lemma/to_json' do
-  @word = Word.first(:lemma => params[:lemma])
-  "#{@word.to_json}"
-end
-
-get '/word/:lemma/to_xml' do
-  @word = Word.first(:lemma => params[:lemma])
-  "#{@word.to_xml}"
-end
-
 get '/stylesheets/style.css' do
   header 'Content-Type' => 'text/css; charset=utf-8'
   sass :style
